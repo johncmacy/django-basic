66b482b82c3228912561d9cb2d774eb46d23c4f1
John Macy <36553266+johncmacy@users.noreply.github.com>
2021-09-03 12:55:23 -0500

    added login and logout urls

--- project/settings.py
+++ project/settings.py
@@ -91,6 +91,9 @@ AUTH_PASSWORD_VALIDATORS = [
 
 AUTH_USER_MODEL = 'users.User'
 
+LOGIN_REDIRECT_URL = 'index'
+LOGOUT_REDIRECT_URL = 'index'
+
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
--- project/urls.py
+++ project/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
-    path('users/', include('users.urls')),
+    path('', TemplateView.as_view(template_name='index.html'), name='index'),
+    path('', include('users.urls')),
     path('admin/', admin.site.urls),
 ]
